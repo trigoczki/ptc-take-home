367d15e0878643d98c304f104a6508a6b86af15a
Tamas Rigoczki <rigoczkitamas@gmail.com>
2026-04-22 12:47:39 +0200

    chore: add backend skeleton

---
+++ src/main/java/me/trigoczki/contenttree/ContentTreeApplication.java
@@ -0,0 +1,12 @@
+package me.trigoczki.contenttree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentTreeApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(ContentTreeApplication.class, args);
+    }
+}
